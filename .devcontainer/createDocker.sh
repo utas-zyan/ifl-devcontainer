0efd2c122499d538e3d343ad18cd864ef1f4648c
Cheney Yan <cheney.yan@gmail.com>
2024-06-27 01:40:57 +0000

    regular update

--- .devcontainer/createDocker.sh
+++ .devcontainer/createDocker.sh
@@ -4,10 +4,11 @@ cp /tmp/ca-bundle.trust.crt /etc/ssl/certs/
 cp /tmp/ca-bundle.crt /etc/ssl/certs/
 
 yum update -y
+yum install yum-utils -y
+yum-config-manager -y --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum install -y epel-release
-yum install -y wget make python3 python3-pip jq  unzip docker
+yum install -y wget make python3 python3-pip jq  unzip docker yum terraform make
 pip3 install awscli
-
 # cheney's own tools
-yum install -y the_silver_searcher vim tmux 
+yum install -y the_silver_searcher vim tmux git
 
