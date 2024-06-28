e6051df30e376939a9ffa95babae137a59dff425
Cheney Yan <cheney.yan@gmail.com>
2024-06-28 17:47:34 +1000

    add more tools

--- .devcontainer/createDocker.sh
+++ .devcontainer/createDocker.sh
@@ -10,5 +10,12 @@ yum install -y epel-release
 yum install -y wget make python3 python3-pip jq  unzip docker yum terraform make
 pip3 install awscli
 # cheney's own tools
-yum install -y the_silver_searcher vim tmux git
+yum install -y the_silver_searcher vim tmux git zip
+# sdkman
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 
+# groovy
+yum install -y java-11-openjdk #java-11-openjdk-devel
+source $HOME/.bashrc
+sdk install groovy
